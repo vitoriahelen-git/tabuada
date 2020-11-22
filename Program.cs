3416a8f6716f349fd18938ed5e29a78ae50876ba
Vitoria <vitoriahellen3004@gmail.com>
2020-11-22 20:14:07 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Tabuada"); 
+            Console.Write("Digite um numero inteiro: "); 
+            numero = Convert.ToInt32(Console.ReadLine());
+            
+            for (int x = 0; x<=10; x++)
+            {
+                Console.WriteLine($"{numero} x {x} = " + ((numero * x)));  
+            }
+        }     
+    }
+}
